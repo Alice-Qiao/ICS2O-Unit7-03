dca98ef11e1c13c9410452617110a61f3c1cc4c2
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-24 15:06:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let yes = 0
+// this means that number will be whatever the user inputs
+
+document.getElementById('submit').addEventListener('click', compare)
+// will activate function compare when button is clicked
+
+function compare () {
+  yes = document.getElementById('age').value
+  if (yes >= 17) {
+    document.getElementById('seventeen').style.display = 'block'
+ } else if (yes >= 13) {
+    document.getElementById('thirteen').style.display = 'block'
+ } else if (yes >= 5) {
+    document.getElementById('five').style.display = 'block'
+ } else if (yes < 5) {
+    document.getElementById('zero').style.display = 'block'
+ }
+}
+// 
